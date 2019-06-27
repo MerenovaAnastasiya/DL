16bd4b29f475936fee1448de69cce05e1fa9f03c
M.Aider <ideeer4@gmail.com>
2019-06-27 03:03:23 +0300

    REMOVE redudant fiels and update default App function

--- src/main/resources/templates/src/App.js
+++ src/main/resources/templates/src/App.js
@@ -1,50 +1,16 @@
 import React from 'react';
-import { Navbar, Footer, Modal } from 'react-materialize';
-import RegisterForm from './components/RegisterForm';
-import BookShelf from './components/BookShelf';
-import Main from './components/Main';
-import M from './materialize';
-import { Route, Switch, Link } from 'react-router-dom';
+import Top from "./containers/Top";
+import Mid from "./containers/Mid";
+import Bot from "./containers/Bot";
 
-class App extends React.Component{
-
-    constructor(props){
-        super(props);
-    }
-
-    componentDidMount() {
-        M.Modal.init(this)
-    }
-
-    render() {
-        return (
-            <div className='app'>
-                <Navbar className='topBar'
-                        alignLinks="right">
-                    <Modal header="Log in or register" trigger={<Link to='/registration'> Registration </Link>}>
-                        <RegisterForm/>
-                    </Modal>
-                </Navbar>
-                <Switch>
-                    <Route path='/' render={() => {
-                        return (<div><Main/><BookShelf/></div>)
-                    }}/>
-                </Switch>
-                <Footer
-                    copyrights="&copy 2019 Copyright Text"
-                    links={<ul/>}
-                    className="brown"
-                >
-                    <h5 className="white-text">
-                        Footer Content
-                    </h5>
-                    <p className="grey-text text-lighten-4">
-                        You can use rows and columns here to organize your footer content.
-                    </p>
-                </Footer>
-            </div>
-        );
-    }
-}
+const App = () => {
+    return (
+        <div className='app'>
+            <Top/>
+            <Mid/>
+            <Bot/>>
+        </div>
+    );
+};
 
 export default App;
--- src/main/resources/templates/src/index.js
+++ src/main/resources/templates/src/index.js
@@ -2,11 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
+import configureStore from './store/ConfigureStore';
+
+const store = configureStore();
 
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+      <Provider store={store}>
+          <App />
+      </Provider>
   </BrowserRouter>,
    document.getElementById('root')
 );
